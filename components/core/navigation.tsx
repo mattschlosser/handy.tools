33bf95f01f3a239fa8306b265ccadac4e088b31f
MantasMikal <mantas.codes@gmail.com>
2024-10-01 18:41:20 +0300

    fix: make nav on top

--- components/core/navigation.tsx
+++ components/core/navigation.tsx
@@ -4,7 +4,7 @@ import { ModeToggle } from "../ui/mode-toggle";
 
 export function Navigation() {
   return (
-    <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 w-full">
+    <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 w-full z-50">
       <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <Link
           href="/"
