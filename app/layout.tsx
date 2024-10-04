f3d03fc800bda880a63a4531da028aa32e184c2e
MantasMikal <mantas.codes@gmail.com>
2024-10-04 15:01:41 +0300

    feat: add meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Lexend } from "next/font/google";
-import "./globals.css";
 import { Navigation } from "@/components/core/navigation";
 import ThemeProvider from "@/components/theme-provider";
+
+import "./globals.css";
 
 const sansFont = Lexend({
   variable: "--font-sans",
@@ -11,7 +12,57 @@ const sansFont = Lexend({
 
 export const metadata: Metadata = {
   title: "Handy Tools",
-  description: "Handy tools for everyday use",
+  description: "Tools for developers",
+  icons: [
+    {
+      url: "/favicon.ico",
+      rel: "icon",
+      type: "image/x-icon",
+    },
+    {
+      url: "/android-chrome-192x192.png",
+      rel: "icon",
+      type: "image/png",
+      sizes: "192x192",
+    },
+    {
+      url: "/android-chrome-512x512.png",
+      rel: "icon",
+      type: "image/png",
+      sizes: "512x512",
+    },
+    {
+      url: "/apple-touch-icon.png",
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+    },
+    {
+      url: "/favicon-16x16.png",
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+    },
+    {
+      url: "/favicon-32x32.png",
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+    },
+    {
+      url: "/site.webmanifest",
+      rel: "manifest",
+    },
+  ],
+  openGraph: {
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Handy Tools",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
