c20b4235702ae5cddcfec1405b1b871239d9e8e5
MantasMikal <mantas.codes@gmail.com>
2025-02-22 14:10:33 +0300

    fix: add robots txt

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,6 +45,7 @@ export const metadata: Metadata = {
     "msapplication-TileColor": "#262626",
     "msapplication-TileImage": "/mstile-256x256.png",
   },
+  robots: "index, follow",
   keywords: [
     "tools",
     "developer",
