97900006a9e900c7297d9505d70c8ef4ce2000d4
MantasMikal <mantas.codes@gmail.com>
2025-02-23 19:54:40 +0300

    feat: update meta

--- app/image-compressor/page.tsx
+++ app/image-compressor/page.tsx
@@ -1,5 +1,21 @@
 import ImageCompressor from "@/features/image-compression/image-compressor";
 
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  description:
+    "Compress and convert images between JPEG, PNG, WEBP, AVIF, and JXL formats",
+  keywords: [
+    "image compression online",
+    "image optimization",
+    "jpeg compression",
+    "png compression",
+    "webp compression",
+    "avif compression",
+    "jxl compression",
+  ],
+};
+
 export default function Dashboard() {
   return (
     <main className="grow mx-auto flex flex-col w-full p-4 md:p-6 max-w-screen-2xl md:max-h-[calc(100dvh-64px)]">
