7d5002d44c8aaa54a42866536cbde8b6470f1820
Matt Schlosser <mattschlosser@users.noreply.github.com>
2025-04-17 09:12:36 -0600

    fix: update gh link

--- components/core/navigation.tsx
+++ components/core/navigation.tsx
@@ -88,7 +88,7 @@ export function Navigation() {
 
       <div className="ml-auto flex gap-2">
         <Button variant="outline" size="icon" asChild>
-          <Link target="_blank" rel="noopener noreferrer" href="https://github.com/MantasMikal/handy.tools">
+          <Link target="_blank" rel="noopener noreferrer" href="https://github.com/mattschlosser/handy.tools">
             <GitHubLogoIcon className="size-5" />
           </Link>
         </Button>
